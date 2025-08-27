686186eca76c3273587ae58e5787ad54b554244d
Mona Eikli Andresen <monaeik@hotmail.com>
2025-08-27 12:53:19 +0200

    Mona Eikli

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
+const listEl = document.querySelector('.cards');
 
-console.log(data);
+const capitalize = (s) => (s && s.length ? s[0].toUpperCase() + s.slice(1) : s);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+function createCard(pokemon) {
+const li = document.createElement('li');
+li.className = 'card';
+
+
+const h2 = document.createElement('h2');
+h2.className = 'card--title';
+h2.textContent = capitalize(pokemon.name);
+
+
+const img = document.createElement('img');
+img.width = 256;
+img.className = 'card--img';
+const art = pokemon?.sprites?.other?.["official-artwork"]?.front_default;
+img.src = art || pokemon?.sprites?.front_default || '';
+img.alt = `${pokemon.name} artwork`;
+
+
+const ul = document.createElement('ul');
+ul.className = 'card--text';
+
+
+pokemon.stats.forEach((s) => {
+const liStat = document.createElement('li');
+const label = s.stat.name.replace(/-/g, ' ').toUpperCase();
+liStat.textContent = `${label}: ${s.base_stat}`;
+ul.appendChild(liStat);
+});
+
+
+li.append(h2, img, ul);
+return li;
+}
+
+
+// Render all cards
+(function render() {
+if (!Array.isArray(window.data)) return;
+const fragment = document.createDocumentFragment();
+data.forEach((p) => fragment.appendChild(createCard(p)));
+listEl.appendChild(fragment);
+})();
